a31a76665a2bf1d13154e905e2887067f6453033
RIZWAN <morizwan1410@gmail.com>
2026-02-19 15:05:37 +0530

    Spring Security - JwtUtils (Generating Token form username)

--- src/main/java/com/url/shortener/security/jwt/JwtUtils.java
+++ src/main/java/com/url/shortener/security/jwt/JwtUtils.java
@@ -1,9 +1,24 @@
 package com.url.shortener.security.jwt;
 
+import com.url.shortener.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JwtUtils
 {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
     public String getJwtFromHeader(HttpServletRequest request)
     {
         String bearerToken = request.getHeader("Authorization");
@@ -13,4 +28,26 @@ public class JwtUtils
         }
         return null;
     }
+
+    public String generateToken(UserDetailsImpl userDetails)
+    {
+        String username = userDetails.getUsername();
+        String roles = userDetails.getAuthorities().stream()
+                .map(authority -> authority.getAuthority())
+                .collect(Collectors.joining(","));
+
+        return Jwts.builder()
+                .subject(username)
+                .claim("roles", roles)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date().getTime() + jwtExpirationMs)))
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key()
+    {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
 }
