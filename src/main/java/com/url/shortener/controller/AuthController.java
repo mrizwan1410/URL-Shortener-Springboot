877d9995ddeca6375d34e413f1bedc9743ad058b
RIZWAN <morizwan1410@gmail.com>
2026-02-22 17:22:17 +0530

    Fix- DI UserService

--- src/main/java/com/url/shortener/controller/AuthController.java
+++ src/main/java/com/url/shortener/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.url.shortener.controller;
 import com.url.shortener.dtos.RegisterRequest;
 import com.url.shortener.models.User;
 import com.url.shortener.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController
 {
     private UserService userService;
@@ -21,6 +23,7 @@ public class AuthController
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setPassword(registerRequest.getPassword());
+        user.setEmail(registerRequest.getEmail());
         user.setRole("ROLE_USER");
 
         userService.registerUser(user);
--- src/main/java/com/url/shortener/security/WebSecurityConfig.java
+++ src/main/java/com/url/shortener/security/WebSecurityConfig.java
@@ -44,6 +44,7 @@ public class WebSecurityConfig
         return authProvider;
     }
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
         http.csrf(AbstractHttpConfigurer::disable)
