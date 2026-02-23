0bcf75edd4bbd036ddea42385d78116f57ad0305
RIZWAN <morizwan1410@gmail.com>
2026-02-23 16:58:52 +0530

    Login endpoint

--- src/main/java/com/url/shortener/security/WebSecurityConfig.java
+++ src/main/java/com/url/shortener/security/WebSecurityConfig.java
@@ -5,7 +5,9 @@ import com.url.shortener.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,6 +36,12 @@ public class WebSecurityConfig
     public PasswordEncoder passwordEncoder()
     {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+    {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
